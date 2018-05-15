c1855c1ae3540154800fe61791108b51025de936
brn.developers <brn.developers@achiever-26.local>
2018-05-15 10:41:22 +0530

    hai ashok 1.0

---
+++ githubashok/githubashok/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  githubashok
+//
+//  Created by brn.developers on 5/15/18.
+//  Copyright © 2018 brn.developers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hai ashok")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
